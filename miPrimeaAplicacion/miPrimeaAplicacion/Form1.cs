218527edb3d0c184be911f5ce96058ffc7d7acf7
Steven Maravilla <stevenmaravilla62@gmail.com>
2026-09-23 10:28:40 -0600

    Add files via upload

---
+++ miPrimeaAplicacion/miPrimeaAplicacion/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miPrimeaAplicacion {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnAgregarAlumno_Click(object sender, EventArgs e)
+        {
+            if (btnAgregarAlumno.Text == "Agregar") {
+                btnAgregarAlumno.Text = "Guardar";
+                btnModificarALumno.Text = "Cancelar";
+            }
+            else {//Guardar
+
+                btnAgregarAlumno.Text = "Agregar";
+                btnModificarAlumno.Text = "Modificar";
+            }
+        }
+        private void btnModificarAlumno_Click(object sender, EventArgs e)
+        {
+            if (btnModificarAlumno.Text == "Modificar")
+            {
+                btnAgregarAlumno.Text = "Guardar";
+                btnModificarALumno.Text = "Cancelar";
+            }
+            else
+            {//Guardar
+
+                btnAgregarAlumno.Text = "Agregar";
+                btnModificarAlumno.Text = "Modificar";
+            }
+        }
